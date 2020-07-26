e7971d53dd80cc204e6d746ba0e527a46d8d926c
hwayn <hwayning@gmail.com>
2020-07-26 18:02:48 +0800

    update

--- practice/ex03/__tests__/index.spec.js
+++ practice/ex03/__tests__/index.spec.js
@@ -1,5 +1,6 @@
 test('练习03 文件流实现JSON读取',async () => {
     const { parser } = require('../index')
     const { user } = await parser(__dirname + '/data/data.json')
+    console.log(user)
     expect(user).toBe('tom');
 })
